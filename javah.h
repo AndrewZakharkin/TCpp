0e326ad229ea78e98c396cae63aca8b6294abfac
Andrew Zakharkin <zakharking@gmail.com>
2018-06-30 12:35:12 +0300

    JNI Linux test

--- javah.h
+++ javah.h
@@ -4,5 +4,23 @@
 
 #ifndef FIRSTTEST_JAVAH_H
 #define FIRSTTEST_JAVAH_H
+#include <jni.h>
+/* Header for class com_zakharkin_Main_Entry */
 
+#ifndef _Included_com_zakharkin_Main_Entry
+#define _Included_com_zakharkin_Main_Entry
+#ifdef __cplusplus
+extern "C" {
+#endif
+/*
+ * Class:     com_zakharkin_Main_Entry
+ * Method:    tst
+ * Signature: (Ljava/lang/String;)V
+ */
+JNIEXPORT void JNICALL Java_com_zakharkin_Main_Entry_tst(JNIEnv * env, jclass cls, jstring str);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
 #endif //FIRSTTEST_JAVAH_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
-
-int main() {
+#include <jni.h>
+#include "javah.h"
+/*int main() {
     std::cout << "Hello, World!" << std::endl;
+
     return 0;
+}*/
+
+JNIEXPORT void JNICALL Java_com_zakharkin_Main_Entry_tst(JNIEnv * env, jclass cls, jstring str)
+{
+    jboolean isCopy;
+    const char* chrs = env->GetStringUTFChars(str, &isCopy);
+    std::cout << chrs << std::endl;
+    env->ReleaseStringUTFChars(str, chrs);
 }
